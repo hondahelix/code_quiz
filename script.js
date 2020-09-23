9bbcf5b0fffe8b9e849907b08085fe8ae4a3710f
Jonathan Honda <hondahelix120@gmail.com>
2020-09-22 20:59:46 -0700

    got the code to check if the button that was clicked = the answer

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ var Quizquestion = document.querySelector("#question-title");
 var b = [document.querySelector("#button-1"),
         document.querySelector("#button-2"),
         document.querySelector("#button-3"),
-        document.querySelector("#button-4"),];
+        document.querySelector("#button-4")
+    ];
+var answer = document.querySelector("#correctAnswer");
 
 var endscreen = document.querySelector("#endscreen");
 console.log(b);
@@ -17,6 +19,11 @@ var questions = [
         title:"what is my name?",
         choices: ["Jon","Bob","Faran","Joe"],
         answer: "Faran"
+    },
+    {
+        title:"yoyoyo",
+        choices: ["a","e","z","o"],
+        answer: "z"
     },
 ];
 
@@ -35,27 +42,38 @@ function setQandA(QsetIndex){
     for(var i =0; i<c.length;i++){
         b[i].value=c[i];
         b[i].textContent=c[i];
-        console.log(b[i].value);
+        //console.log(b[i].value);
     }
 }
 
-function checkIfRight(){
-    
+function checkIfAnswer(objButton){
+    for(var i=0; i<questions.length;i++){
+        setQandA(i);
+        if(objButton.value==questions[i].answer){
+            //console.log("rigth");
+            answer.textContent = "Right";
+        }
+        else if(objButton.value!==questions[i].answer){
+            //console.log("wrong");
+            answer.textContent = "Wrong";
+        }
+    }
 }
 
 
+
 startButton.addEventListener("click", function() {
+    
     hidescreen(startScreen);
     questionScreen.style.display = "block";
 });
-questionScreen.addEventListener("click", function() {
-    //hidescreen(questionScreenn);
-});
+// questionScreen.addEventListener("click", function() {
+//     //hidescreen(questionScreenn);
+// });
 endscreen.addEventListener("click", function() {
     hidescreen(endscreen);
 });
-
-setQandA(0)
+//getIdOfButton();
+setQandA(0);
 console.log(b[0].value);
-
 console.log("connected");
